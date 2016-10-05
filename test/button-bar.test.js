47274999c8bf12dbbd083199974169a8f70a68ff
garthdb <garthdb@gmail.com>
2016-10-05 16:53:20 -0600

    feat(Build): switched build to Resin
    BREAKING CHANGE:
    
    Changed build process to Resin

--- test/button-bar.test.js
+++ test/button-bar.test.js
@@ -9,66 +9,66 @@ describe('Topcoat button bar', function() {
   'use strict';
 
   it('should output correct desktop light css', function() {
-    var actual = read('css/desktop-light.css');
-    var expected = read('test/expected/desktop-light.css');
+    var actual = read('css/desktop-light-button-bar.css');
+    var expected = read('test/expected/desktop-light-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('should output correct desktop dark css', function() {
-    var actual = read('css/desktop-dark.css');
-    var expected = read('test/expected/desktop-dark.css');
+    var actual = read('css/desktop-dark-button-bar.css');
+    var expected = read('test/expected/desktop-dark-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('should output correct mobile light css', function() {
-    var actual = read('css/mobile-light.css');
-    var expected = read('test/expected/mobile-light.css');
+    var actual = read('css/mobile-light-button-bar.css');
+    var expected = read('test/expected/mobile-light-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('should output correct mobile dark css', function() {
-    var actual = read('css/mobile-dark.css');
-    var expected = read('test/expected/mobile-dark.css');
+    var actual = read('css/mobile-dark-button-bar.css');
+    var expected = read('test/expected/mobile-dark-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('mobile light should not have any unrendered variables', function() {
-    var actual = read('css/mobile-light.css');
+    var actual = read('css/mobile-light-button-bar.css');
     assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
   });
 
 
   it('should output correct mobile dark css', function() {
-    var actual = read('css/mobile-dark.css');
-    var expected = read('test/expected/mobile-dark.css');
+    var actual = read('css/mobile-dark-button-bar.css');
+    var expected = read('test/expected/mobile-dark-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('mobile dark should not have any unrendered variables', function() {
-    var actual = read('css/mobile-dark.css');
+    var actual = read('css/mobile-dark-button-bar.css');
     assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
   });
 
   it('should output correct desktop dark css', function() {
-    var actual = read('css/desktop-dark.css');
-    var expected = read('test/expected/desktop-dark.css');
+    var actual = read('css/desktop-dark-button-bar.css');
+    var expected = read('test/expected/desktop-dark-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('desktop dark should not have any unrendered variables', function() {
-    var actual = read('css/desktop-dark.css');
+    var actual = read('css/desktop-dark-button-bar.css');
     assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
   });
 
 
   it('should output correct desktop light css', function() {
-    var actual = read('css/desktop-light.css');
-    var expected = read('test/expected/desktop-light.css');
+    var actual = read('css/desktop-light-button-bar.css');
+    var expected = read('test/expected/desktop-light-button-bar.css');
     assert.equal(actual, expected, 'should generate correct css');
   });
 
   it('desktop light should not have any unrendered variables', function() {
-    var actual = read('css/desktop-light.css');
+    var actual = read('css/desktop-light-button-bar.css');
     assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
   });
 
